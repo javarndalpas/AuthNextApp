dd8dd5b6627d6fcae7d63f3daa4bedcd839c83ba
alpas <alpasshrivastava.javarnd@gmail.com>
2025-05-17 03:21:34 +0530

    code to dev

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,30 @@
-export default function SignUp() {
+import React from "react"
 
+export default function SignUpPage() {
+
+    const [user, setUser] = React.useState({
+        email: "",
+        password: "",
+        username: ""
+    })
+
+    const onSignUp = () => {
+
+    }
 
     return (
         <>
-            Signup Page
+            <div className="flex">
+                <h1>SignUp</h1>
+                <hr />
+                <label htmlFor="username">UserName</label>
+                <input type="text"
+                    id="username"
+                    value={user.username}
+                    onChange={(e) => setUser({ ...user, username: e.target.value })}
+                    placeholder="UserName"
+                />
+            </div>
         </>
     )
 }
