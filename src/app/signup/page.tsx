d6d9f17efe9612aa7a8a2d5870a3abd83c45dca1
alpas <alpasshrivastava.javarnd@gmail.com>
2025-06-06 03:25:47 +0530

    added signup changes

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,35 +1,34 @@
 "use client"
 import Link from "next/link"
-import React from "react"
+import { useRouter } from "next/navigation";
+
+import React, { useEffect } from "react"
 
 export default function SignUpPage() {
-
+    const router = useRouter();
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
     const [user, setUser] = React.useState({
         email: "",
         password: "",
         username: ""
     })
 
-    const onSignUp = () => {
+    const onSignUp = async () => {
 
     }
+
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+            setButtonDisabled(false)
+        } else {
+            setButtonDisabled(true);
+        }
+    }, [user])
+
 
     return (
         <>
             <div >
-                {/* <div className="flex flex-col items-center w-3xl text-center justify-center min-h-screen py-2">
-                <h1>SignUp</h1>
-                <hr />
-                <label htmlFor="username">UserName</label>
-                <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    type="text"
-                    id="username"
-                    value={user.username}
-                    onChange={(e) => setUser({ ...user, username: e.target.value })}
-                    placeholder="UserName"
-                />
-            </div> */}
-
                 <form className="max-w-md mx-auto mt-18">
                     <h1 >Signup</h1>
 
@@ -50,11 +49,6 @@ export default function SignUpPage() {
                         <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                         <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                     </div>
-                    {/* =========================confirm password=============== */}
-                    {/* <div className="relative z-0 w-full mb-5 group">
-                        <input type="password" name="repeat_password" id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-                        <label htmlFor="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
-                    </div> */}
 
                     <div className="grid md:grid-cols-2 md:gap-6">
                         <div className="relative z-0 w-full mb-5 group">
@@ -73,10 +67,9 @@ export default function SignUpPage() {
                             <label htmlFor="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
                         </div>
                     </div>
-                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                    <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{buttonDisabled ? "No Signup" : "Signup Now"}</button>
                     <h1>Already have an account? <Link className="text-blue-600" rel="stylesheet" href="/login">login</Link> </h1>
                 </form>
-
             </div>
         </>
     )
