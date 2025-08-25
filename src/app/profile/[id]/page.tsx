665293e07fa76eea64c64f3964edab9d6c63ad0e
alpas <alpasshrivastava.javarnd@gmail.com>
2025-08-26 03:30:52 +0530

    changes

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,9 +1,9 @@
 export default function UserProfile({params} : any) {
     return (
     <>
-    <h1>profile COmp</h1>
+    <h1 className="p-4">profile Details Page </h1>
     <hr />
-    <h2>profile id page {params.id}</h2>
+    <h2 className="p-4">profile id page <span className="bg-amber-200 p-2"> {params.id}</span></h2>
     </>
 )
 }
