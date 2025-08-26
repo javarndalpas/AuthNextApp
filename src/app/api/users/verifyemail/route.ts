238f6ca45016301ee3e5d91a2f6695a9c7301b33
alpas <alpasshrivastava.javarnd@gmail.com>
2025-08-27 03:14:23 +0530

    email notification

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,31 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+connect();
+export async function POST(request: NextRequest) {
+
+    try {
+        const reqbody = await request.json();
+        const { token } = reqbody;
+        console.log(token);
+        const user = await User.findOne({
+            verifyToken: token,
+            verifyTokenExpiry: { $gt: Date.now() }
+        });
+
+        console.log("enter", user);
+        if (!user) {
+            return NextResponse.json({ error: "invalid Token" },
+                { status: 400 }
+            )
+        }
+        console.log(user, "=====");
+
+        user.verifyToken === undefined;
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message },
+            { status: 500 }
+        )
+    }
+}
